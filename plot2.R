f1479a51997a2be651f606338c5a52f38a01810d
prakashj <prakash.j@gmail.com>
2014-05-07 02:08:30 -0500

    updated

--- plot2.R
+++ plot2.R
@@ -3,7 +3,7 @@ plot2 <- function ()
   hospfile <- read.csv("household_subset.txt",sep=";") #Reading the subset file for 2 days
   gapk <- hospfile$Global_active_power
   timecol <- do.call(paste,c(hospfile[c("Date","Time")], sep=" "))
-  timecol <- strptime(timecol, "%m/%d/%Y %H:%M:%S")
+  timecol <- strptime(timecol, "%d/%m/%Y %H:%M:%S")
   png("plot2.png")
   plot(timecol,gapk,type="l",xlab="",ylab="Global Active Power (kilowatts)", yaxt ="n", ylim=c(0,8))
   axis(2,at=seq(0,6,by=2))
