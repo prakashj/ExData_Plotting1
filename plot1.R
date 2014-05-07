49fb43c8d1ff1597e75a513201a6d0b6222d676e
prakashj <prakash.j@gmail.com>
2014-05-07 00:36:19 -0500

    updated graph

--- plot1.R
+++ plot1.R
@@ -1,7 +1,8 @@
 plot1 <- function ()
 {
-  hospfile <- read.csv("household_subset.txt",sep=";")
+  hospfile <- read.csv("household_subset.txt",sep=";") #Reading the subset file for 2 days
   png("plot1.png")
-  hist(hospfile$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
+  hist(hospfile$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red",xlim=c(0,8),xaxt="n")
+  axis(1,at=seq(0,6,by=2))
  dev.off()
 }
